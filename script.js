f4b839734dae7ff1d1ade6cef690fd823aa52714
Arjun singh gurjar <115486632+Arjun09091999@users.noreply.github.com>
2023-06-29 12:58:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function delay(message, time) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(message);
+    }, time);
+  });
+}
+
+async function displayMessage() {
+  const message = await delay('Hello, world!', 2000);
+  document.getElementById('output').textContent = message;
+}
+
+displayMessage();
+
